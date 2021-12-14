31c31d220b36e1b4bf9d7c1a386e1364b247f76e
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2021-12-14 14:22:53 +0300

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-string ride_id;
-    // string rideable_type;
-    // string start_date;
-    // string start_time;
-    // string start_station_id;
-    // string start_station_name;
-    // string start_station_lat;
-    // string start_station_long;
-    // string end_date;
